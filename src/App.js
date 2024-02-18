484388b4911fd4c8fb1a4b97cd7e1dbc27036b97
Thomas Carvalho <thomascaraujo19@gmail.com>
2024-02-18 02:37:40 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState('dark');
+  
+  const incrementCount = () =>{
+    setCount((prevState) => prevState + 1);
+    // Para pegar o valor anterior e modifica-lo
+  };
+
   return (
     <div>
-      
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>Increment</button>
     </div>
   );
 }
